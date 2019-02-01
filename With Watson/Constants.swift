426e6e670368bff2a06f3b644d5bd41d0de9bcf0
Victor Kazuyuki Shinya <contato@victorshinya.com.br>
2019-02-01 18:40:56 -0200

    Remove model list on Constants

--- With Watson/Constants.swift
+++ With Watson/Constants.swift
@@ -13,5 +13,4 @@ class Constants {
     static let version = "2018-03-19" // The current version is 2018-03-19
     static let apikey = "" // Set apikey from IBM Watson Visual Recognition service
     static let api_key = "" // Set api_key from IBM Watson Visual Recognition service. Instances before May 23, 2018 uses an old api_key. Any instances created after May 22 use an IAM 'apikey'
-    static let models = [""] // Set all model ids from Visual Recognition model training
 }
